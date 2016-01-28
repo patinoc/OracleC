ccce52b9f00e7490b28607f6b3dc4bb5748d2994
Christian <cdp13c@my.fsu.edu>
2016-01-28 02:40:39 -0500

    Update HardwoodSellerC.cpp
    Changes:
    - More comments for functions
    - Update for command line usage

--- src/HardwoodSellerC.cpp
+++ src/HardwoodSellerC.cpp
@@ -7,11 +7,15 @@
 //============================================================================
 
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <vector>
 #include <map>
 #include "WoodItem.h"
 
+/*
+* Contains Customer's full name, address, and date of the order.
+*/
 struct Customer
 {
 	std::string name, address, date;
@@ -24,9 +28,8 @@ double deliveryMultiplier(int bfamount);
 double deliveryTime(const std::vector<WoodItem>&);
 
 /*
- * Contains Customer's full name, address, and date of the order.
- */
-
+ * Tables for each wood item's base price and delivery time
+*/
 std::map<std::string, double> BASE_PRICE = { { "Cherry", 5.95 },
 { "Curly Maple", 6.0 }, { "Genuine Mahogany", 9.6 }, { "Wenge", 22.35 },
 { "White Oak", 6.7 }, { "Sawdust", 1.5 } };
@@ -34,15 +37,17 @@ std::map<std::string, double> BASE_DELIVERY_TIME = { { "Cherry", 2.5 },
 { "Curly Maple", 1.5 }, { "Genuine Mahogany", 3.0 }, { "Wenge", 5.0 },
 { "White Oak", 2.3 }, { "Sawdust", 1.0 } };
 
-int main() {
+int main(int argc, char* argv[]) {
+	if (argc != 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " filename\n";
+		exit(1);
+	}
 	Customer customer;
 	std::vector<WoodItem> orders;
-	readInputFile("test.txt", customer, orders);
-	for (const WoodItem& w : orders)
-	{
-		std::cout << w.type << ' ' << w.price << ' ' << w.baseDeliveryTime << '\n';
-	}
-	std::cout << "\n\n" << deliveryTime(orders) << '\n';
+
+	readInputFile(std::string(argv[1]), customer, orders);
+
 	return 0;
 }
 
@@ -69,6 +74,11 @@ void readInputFile(std::string inputFilePath, Customer& customer, std::vector<Wo
 
 }
 
+
+/* 
+ * Reads string of the form "<name>;<address>;<date>"
+ * and populates a struct with the information
+ */
 void parseCustomer(std::string line, Customer& customer) {
 	std::string token;
 	std::stringstream ss(line);
@@ -80,6 +90,10 @@ void parseCustomer(std::string line, Customer& customer) {
 	customer.date = token;
 }
 
+/*
+* Reads string of the form "<WoodName>:<amount>;...;<WoodName>:<amount>"
+* and populates a vector of WoodItems with relevant information
+*/
 void parseOrders(std::string line, std::vector<WoodItem>& orders)
 {
 	std::stringstream ss(line);
@@ -97,6 +111,11 @@ void parseOrders(std::string line, std::vector<WoodItem>& orders)
 	}
 }
 
+/*
+ * Input: bfamount, the amount of Board Foot
+ * Output: The multiplier associated with the range the BF
+ *         falls under.
+ */
 double deliveryMultiplier(int bfamount)
 {
 	if (bfamount <= 100)
